2d1c981f5414800e459673266a75f7df61547e90
fbpereira <69329281+fbpereira@users.noreply.github.com>
2020-08-06 21:30:44 -0300

    Update cachematrix.R
    Programming Assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix Inversion
+## The following functions are able to give you the inverse of a square matrix.
 
-## Write a short comment describing this function
+## The makeCacheMatrix is a function that will create a special "matrix" object that can cache its inverse.
+## Attention: create an object to attribute the special "matrix" object. It will be used in the next function.
 
 makeCacheMatrix <- function(x = matrix()) {
+  m.inverse <- NULL
+        set <- function(y){ 
+                x <<-y
+                m.inverse <<- NULL}
+        get <- function() x
+        set_inverse <- function(inverse) m.inverse <<- inverse
+        get_inverse <- function() m.inverse
+        list(set = set, get = get,
+             set_inverse = set_inverse,
+             get_inverse = get_inverse)
 
 }
 
 
-## Write a short comment describing this function
+##  This function computes the inverse of the special "matrix" returned by makeCacheMatrix. 
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+## Use the special "matrix" object to obtain the inverse of the original matrix or you can nest the functions (cacheSolve(makeCacheMatrix(x)).
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m.inverse<-x$get_inverse()
+  
+  if(!is.null(m.inverse)){
+    message("Getting cached data")
+    return(m.inverse)}
+  
+  matrix <- x$get()
+  m.inverse <- solve(matrix,...)
+  x$set_inverse(m.inverse)
+  m.inverse
+}
+
+## I believe that is easiear create another function that automatically nest the above functions, so you can use the following:
+
+Inverse_matrix <- function(x,...){
+  cacheSolve(makeCacheMatrix(x))
 }
